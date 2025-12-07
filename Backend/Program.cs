6b4c6a3d893308ca6e67cda95cc528a054cdb3a5
Jesse-Shema-Kyanga <jessbra4@gmail.com>
2025-12-07 03:04:14 +0200

    fixed a line in program.cs that would confuse group mates when trying to test end points out in swagger

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -126,7 +126,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "LendSecure API v1");
-        c.RoutePrefix = string.Empty; // Swagger at root
+        c.RoutePrefix = "swagger"; // Swagger at /swagger
     });
 }
 
